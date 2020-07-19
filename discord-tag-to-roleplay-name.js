36b5a4a68abd0efeb0d2bc7e136774bc12576d09
dubblebubbleguy <68306036+dubblebubbleguy@users.noreply.github.com>
2020-07-19 14:00:05 -0500

    22

---
+++ discord-tag-to-roleplay-name.js
@@ -0,0 +1,30 @@
+var discordTagsToRoleplayNames = {
+	"spartacus#6134": "Franklin Hudson",
+	"old_dude47#3377": "Harold Elliotsone",
+	"AModerateFlipFlop#4974": "Henry Lee S",
+	"drifters🍿#9665": "Drifters",
+	"dubblebubbleguy#8460": "Dubblebubbleguy",
+	"andreasersson05#6619": "Andreas Ersson",
+	"Landers#7367": "Alejandro Landers",
+	"Ive_been_crushed_by_a_giant_bell#8544": "Dennis Edwards",
+	"GodofPolitics#7746": "GodofPolitics",
+	"Nick R#6802": "Nicholas Richardson",
+	"Kaidon Spare For President#2299": "Kaidon Spare",
+};
+
+var politicianLists = document.getElementsByClassName("politician");
+var politicianLists2 = document.getElementsByClassName("politician-no-overlay");
+
+// Find all politician tags, replace their discord tag with their roleplay name and give them an overlay for their discord tag
+for(i=0;i<politicianLists.length;i++) {
+	if(discordTagsToRoleplayNames[politicianLists[i].innerHTML] != null) {
+		politicianLists[i].innerHTML = discordTagsToRoleplayNames[politicianLists[i].innerHTML] + ' <span class="politician-overlay"><i>'+politicianLists[i].innerHTML+'</i></span>';
+	}
+}
+
+// Find all politician tags, replace their discord tag with their roleplay name but don't give them an overlay
+for(i=0;i<politicianLists2.length;i++) {
+	if(discordTagsToRoleplayNames[politicianLists2[i].innerHTML] != null) {
+		politicianLists2[i].innerHTML = discordTagsToRoleplayNames[politicianLists2[i].innerHTML];
+	}
+}
---
+++ house-archive.js
@@ -0,0 +1,6 @@
+// Find all politician tags, replace their discord tag with their roleplay name and give them an overlay for their discord tag
+console.log("HI");
+console.log(x);
+for(i=0;i<politicianData.length;i++) {
+	console.log(politicianData[i].party);
+}
